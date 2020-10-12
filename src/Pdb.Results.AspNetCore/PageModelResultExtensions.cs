c307559b02c332747b35a0b416e3d7f33f565eed
pauldbentley <paul@paulbentley.co.uk>
2020-10-12 12:01:49 +0100

    Resolves issue with null actions. Makes modelPrefix nullable. Adds the errors to the model state on ResultStatus.Error

--- src/Pdb.Results.AspNetCore/PageModelResultExtensions.cs
+++ src/Pdb.Results.AspNetCore/PageModelResultExtensions.cs
@@ -2,36 +2,34 @@
 {
     using System;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Mvc.ModelBinding;
     using Pdb.Results;
 
     public static class PageModelResultExtensions
     {
         public static Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request) =>
-            Result(page, request, null!, null!);
+            Result(page, request, value => { }, null);
 
         public static Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, Action<TResult> ok) =>
-            Result(page, request, ok, null!);
+            Result(page, request, ok, null);
 
-        public static Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, string modelPrefix) =>
-            Result(page, request, null!, modelPrefix);
+        public static Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, string? modelPrefix = null) =>
+            Result(page, request, value => { }, modelPrefix);
 
-        public static Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, Action<TResult> ok, string modelPrefix) =>
+        public static Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, Action<TResult> ok, string? modelPrefix = null) =>
             Result(
                 page,
                 request,
                 value =>
                 {
-                    ok?.Invoke(value);
+                    ok.Invoke(value);
                     return page.Page();
                 },
                 modelPrefix);
 
         public static Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, Func<TResult, IActionResult> ok) =>
-            Result(page, request, ok, null!);
+            Result(page, request, ok, null);
 
-        public static async Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, Func<TResult, IActionResult> ok, string modelPrefix)
+        public static async Task<IActionResult> Result<TResult>(this PageModel page, Func<Task<IResult<TResult>>> request, Func<TResult, IActionResult> ok, string? modelPrefix = null)
         {
             if (page is null)
             {
@@ -54,45 +52,19 @@
             }
 
             var result = await request();
-
-            if (result.Status == ResultStatus.NotFound)
-            {
-                return page.NotFound();
-            }
-
-            if (result.Status == ResultStatus.Forbidden)
-            {
-                return page.Forbid();
-            }
-
-            if (result.Status == ResultStatus.Invalid)
-            {
-                page.ModelState.AddModelErrors(result.ValidationErrors, modelPrefix);
-
-                return page.Page();
-            }
-
-            if (result.Status == ResultStatus.Error)
-            {
-                page.ViewData["Error"] = result.Problem;
-                page.ViewData["Errors"] = result.Errors;
-
-                return page.Page();
-            }
-
-            return ok(result.Value);
+            return GetResultAction(page, result, modelPrefix) ?? ok(result.Value);
         }
 
         public static Task<IActionResult> Result(this PageModel page, Func<Task<IResult>> request) =>
-            Result(page, request, null!, null!);
+            Result(page, request, () => page.Page(), null);
 
         public static Task<IActionResult> Result(this PageModel page, Func<Task<IResult>> request, Func<IActionResult> ok) =>
-            Result(page, request, ok, null!);
+            Result(page, request, ok, null);
 
-        public static Task<IActionResult> Result(this PageModel page, Func<Task<IResult>> request, string modelPrefix) =>
-            Result(page, request, null!, modelPrefix);
+        public static Task<IActionResult> Result(this PageModel page, Func<Task<IResult>> request, string? modelPrefix = null) =>
+            Result(page, request, () => page.Page(), modelPrefix);
 
-        public static async Task<IActionResult> Result(this PageModel page, Func<Task<IResult>> request, Func<IActionResult> ok, string modelPrefix)
+        public static async Task<IActionResult> Result(this PageModel page, Func<Task<IResult>> request, Func<IActionResult> ok, string? modelPrefix = null)
         {
             if (page is null)
             {
@@ -104,13 +76,22 @@
                 throw new ArgumentNullException(nameof(request));
             }
 
+            if (ok is null)
+            {
+                throw new ArgumentNullException(nameof(ok));
+            }
+
             if (!page.ModelState.IsValid)
             {
                 return page.Page();
             }
 
             var result = await request();
+            return GetResultAction(page, result, modelPrefix) ?? ok();
+        }
 
+        private static IActionResult? GetResultAction(PageModel page, IResult result, string? modelPrefix = null)
+        {
             if (result.Status == ResultStatus.NotFound)
             {
                 return page.NotFound();
@@ -123,7 +104,19 @@
 
             if (result.Status == ResultStatus.Invalid)
             {
-                page.ModelState.AddModelErrors(result.ValidationErrors, modelPrefix);
+                foreach (var error in result.ValidationErrors)
+                {
+                    string? key = !string.IsNullOrEmpty(error.Key)
+                        ? modelPrefix != null ? modelPrefix + "." + error.Key : error.Key
+                        : string.Empty;
+
+                    var errorsMessages = error.Value;
+
+                    foreach (string errorMessage in errorsMessages)
+                    {
+                        page.ModelState.AddModelError(key, errorMessage);
+                    }
+                }
 
                 return page.Page();
             }
@@ -133,11 +126,15 @@
                 page.ViewData["Error"] = result.Problem;
                 page.ViewData["Errors"] = result.Errors;
 
+                foreach (var error in result.Errors)
+                {
+                    page.ModelState.AddModelError(string.Empty, error);
+                }
+
                 return page.Page();
             }
 
-            var actionResult = ok?.Invoke() ?? page.Page();
-            return actionResult;
+            return null;
         }
     }
 }
