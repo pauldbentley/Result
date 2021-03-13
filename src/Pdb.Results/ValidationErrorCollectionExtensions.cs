cbcff6fb3c775a4023dd36bc2c2a9490b9971c5d
pauldbentley <paul@paulbentley.co.uk>
2021-03-13 09:51:45 +0000

    Adds the ValidationResult collection extensions

---
+++ src/Pdb.Results/ValidationErrorCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Pdb.Results
+{
+    using System.Collections.Generic;
+
+    public static class ValidationErrorCollectionExtensions
+    {
+        public static void Add(this ICollection<ValidationError> collection, string identifier, string errorMessage)
+        {
+            collection.Add(new ValidationError(identifier, errorMessage));
+        }
+
+        public static void Add(this ICollection<ValidationError> collection, string identifier, params string[] errorMessages)
+        {
+            foreach (var errorMessage in errorMessages)
+            {
+                collection.Add(identifier, errorMessage);
+            }
+        }
+    }
+}
