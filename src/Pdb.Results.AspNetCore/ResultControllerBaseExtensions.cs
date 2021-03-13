c9d1c0caf2c5c474418932892157c67d02760cbc
pauldbentley <paul@paulbentley.co.uk>
2021-03-13 09:04:09 +0000

    Adds the result extensions

---
+++ src/Pdb.Results.AspNetCore/ResultControllerBaseExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Microsoft.AspNetCore.Mvc
+{
+    using System.Linq;
+    using Pdb.Results;
+
+    public static class ResultControllerBaseExtensions
+    {
+        public static IActionResult GetActionResult(this Result result, ControllerBase controller)
+        {
+            if (result.Status == ResultStatus.Ok)
+            {
+                return controller.Ok();
+            }
+
+            if (result.Status == ResultStatus.NotFound)
+            {
+                return controller.NotFound();
+            }
+
+            if (result.Status == ResultStatus.Forbidden)
+            {
+                return controller.Forbid();
+            }
+
+            if (result.Status == ResultStatus.Invalid)
+            {
+                var validationErrors = result
+                    .ValidationErrors
+                    .GroupBy(e => e.Identifier)
+                    .Select(e => new
+                    {
+                        e.Key,
+                        Value = e.Select(e => e.ErrorMessage)
+                    });
+
+                foreach (var error in validationErrors)
+                {
+                    foreach (var errorMessage in error.Value)
+                    {
+                        controller.ModelState.AddModelError(error.Key, errorMessage);
+                    }
+                }
+
+                return controller.ValidationProblem();
+            }
+
+            if (result.Status == ResultStatus.Error)
+            {
+                return controller.BadRequest(result.Problem ?? result.Errors);
+            }
+
+            return controller.BadRequest();
+        }
+    }
+}
---
+++ src/Pdb.Results.AspNetCore/ResultPageModelExtensions.cs
@@ -0,0 +1,65 @@
+﻿namespace Microsoft.AspNetCore.Mvc.RazorPages
+{
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+    using Pdb.Results;
+
+    public static class ResultPageModelExtensions
+    {
+        public static IActionResult GetActionResult(
+            this Result result,
+            PageModel page,
+            string? modelPrefix = null)
+        {
+            if (result.Status == ResultStatus.NotFound)
+            {
+                return page.NotFound();
+            }
+
+            if (result.Status == ResultStatus.Forbidden)
+            {
+                return page.Forbid();
+            }
+
+            if (result.Status == ResultStatus.Invalid)
+            {
+                var validationErrors = result
+                    .ValidationErrors
+                    .GroupBy(e => e.Identifier)
+                    .Select(e => new
+                    {
+                        e.Key,
+                        Value = e.Select(e => e.ErrorMessage)
+                    });
+
+                foreach (var error in validationErrors)
+                {
+                    string? key = !string.IsNullOrEmpty(error.Key)
+                        ? modelPrefix != null ? modelPrefix + "." + error.Key : error.Key
+                        : string.Empty;
+
+                    var errorsMessages = error.Value;
+
+                    foreach (string errorMessage in errorsMessages)
+                    {
+                        page.ModelState.AddModelError(key, errorMessage);
+                    }
+                }
+
+                return page.Page();
+            }
+
+            if (result.Status == ResultStatus.Error)
+            {
+                foreach (var error in result.Errors)
+                {
+                    page.ModelState.AddModelError(string.Empty, error);
+                }
+
+                return page.Page();
+            }
+
+            return page.BadRequest();
+        }
+    }
+}
